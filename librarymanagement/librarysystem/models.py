5e41bf3e6b70c7e84f14ece51f1fa51919b22ce6
Nikita <nikita1.citrusbug@gmail.com>
2022-05-04 17:29:57 +0530

    create models

---
+++ librarymanagement/librarysystem/models.py
@@ -0,0 +1,46 @@
+
+from enum import auto
+from tkinter import CASCADE
+from unicodedata import category
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length= 20)
+    
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length = 50)
+    discription = models.TextField(max_length=500)
+    quantity = models.PositiveIntegerField()
+    category = models.ForeignKey(Category , on_delete= models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+
+
+
+class Author(models.Model):
+    name = models.CharField(max_length = 50)
+    discription = models.TextField(max_length = 500)
+    book = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+class IssuedBooks(models.Model):
+    book = models.ForeignKey(Book , on_delete=models.CASCADE)
+    user_name = models.CharField(max_length = 20)
+    user_email = models.EmailField()
+    user_address = models.TextField(max_length = 50)
+    issued_date = models.DateTimeField(auto)
+    return_date = models.DateTimeField()
+    charge_per_day = models.PositiveIntegerField()
+    total_charge = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.user_name
